4132d7d0f6dfc8b560507a7a8b21c102ba9b3fce
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-11-16 10:28:23 +0100

    Add project files.

---
+++ WebAppMM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppMM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
