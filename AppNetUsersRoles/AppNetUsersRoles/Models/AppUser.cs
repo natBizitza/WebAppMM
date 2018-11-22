c0f21a4199f27b8c2fafe445016987a753a7c50f
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-11-22 11:17:49 +0100

    upd

---
+++ AppNetUsersRoles/AppNetUsersRoles/Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppNetUsersRoles.Models
+{
+    public class AppUser : IdentityUser
+    {
+        //to pick up all the attrs of  Identity User
+        public AppUser() : base()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
