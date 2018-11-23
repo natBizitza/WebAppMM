2109ef1d8e77bb3dbd467429edf65b4f81fc63e9
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-11-23 12:06:35 +0100

    upd

--- AppNetUsersRoles/AppNetUsersRoles/Controllers/HomeController.cs
+++ AppNetUsersRoles/AppNetUsersRoles/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AppNetUsersRoles.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AppNetUsersRoles.Controllers
 {
@@ -22,6 +23,8 @@ namespace AppNetUsersRoles.Controllers
             return View();
         }
 
+        //permissions to the admin
+        [Authorize(Roles="admin")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
